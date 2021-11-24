84feeb1827fa80b03b3cdc83d5bdc7e961eca174
AgathePons <ponsagathe37@gmail.com>
2021-11-24 11:09:02 +0100

    function buildBoard done

--- js/app.js
+++ js/app.js
@@ -3,15 +3,26 @@
 function buildBoard(numberBoard) {
     //Création du Tableau
     var pixelBoard = document.getElementById("pixelBoard");
-    console.log(pixelBoard);
+    //console.log(pixelBoard);
     // Créaction des ligne
-    var newLine = document.createElement("tr");
-    console.log(newLine);
-    newLine.classList.add("boardLine");
+    
+    
 
-    for (var index = 0; index < numberBoard; index++){
+    
+    // on construit x lignes en fonction de numberBoard 
+    for (let index = 0; index < numberBoard; index++) {
+        //
+        var newLine = document.createElement("tr");
+        newLine.classList.add("boardLine");
         pixelBoard.appendChild(newLine);
         console.log(index);
+
+        for (let index = 0; index < numberBoard; index++) {
+            var newCell = document.createElement('td');
+            newCell.classList.add("boardCell");
+            newLine.appendChild(newCell);
+        }
     }
 }
 
+buildBoard(8);
