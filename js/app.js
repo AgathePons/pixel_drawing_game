e1d6dff963de33855e791142efce6c9ba058ea41
BilalTeix <bilal.teix@gmail.com>
2021-11-24 10:27:53 +0100

    Etape 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 // ton code ici
+
+function buildBoard(numberBoard) {
+    //Création du Tableau
+    var pixelBoard = document.getElementById("pixelBoard");
+    console.log(pixelBoard);
+    // Créaction des ligne
+    var newLine = document.createElement("tr");
+    console.log(newLine);
+    newLine.classList.add("boardLine");
+
+    for (var index = 0; index < numberBoard; index++){
+        pixelBoard.appendChild(newLine);
+        console.log(index);
+    }
+}
+
