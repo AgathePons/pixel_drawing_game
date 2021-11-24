eaf296a0cfea1ee2d95e1d264916e4c0b7137777
AgathePons <ponsagathe37@gmail.com>
2021-11-24 14:29:08 +0100

    bonus 1 done

--- js/app.js
+++ js/app.js
@@ -1,39 +1,61 @@
 // ton code ici
+// objet app
+var app = {
+    'headerForm': document.getElementById('header-form'),
+    'gridInput': document.createElement('input'),
+    'pixelInput': document.createElement('input'),
+    'formButton': document.createElement('button'),
 
-function buildBoard(numberBoard) {
-    //Création du Tableau
-    var pixelBoard = document.getElementById("pixelBoard");
-    // Créaction des ligne
+    init() {
+        // recupérer le form et construire dedans les input et le bouton
+        console.log(app.headerForm);
+        //construire les input
+        app.gridInput.id = 'grid-input';
+        app.gridInput.setAttribute('type', 'number');
+        app.gridInput.setAttribute('placeholder', 'Taille de la grille');
+        app.headerForm.appendChild(app.gridInput);
 
-    // on construit x lignes en fonction de numberBoard 
-    for (let index = 0; index < numberBoard; index++) {
-        //
-        var newLine = document.createElement("tr");
-        var idLine = index;
-        newLine.classList.add("boardLine");
-        newLine.id = index;
-        pixelBoard.appendChild(newLine);
+        app.pixelInput.id = 'pixel-input';
+        app.pixelInput.setAttribute('type', 'number');
+        app.pixelInput.setAttribute('placeholder', 'Taille des pixels');
+        console.log(app.pixelInput);
+        app.headerForm.appendChild(app.pixelInput);
 
+        //construire le bouton
+        app.formButton.id = "form-button";
+        app.formButton.setAttribute('type', 'button');
+        app.formButton.textContent = 'Valider';
+        app.headerForm.appendChild(app.formButton);
+    },
+    buildBoard(numberBoard) {
+        //Création du Tableau
+        var pixelBoard = document.getElementById("pixelBoard");
+        // Créaction des ligne
+
+        // on construit x lignes en fonction de numberBoard 
         for (let index = 0; index < numberBoard; index++) {
-            var newCell = document.createElement('td');
-            newCell.classList.add("boardCell");
-            newCell.classList.add("black");
-            newCell.id = `line-${idLine}-cell-${index}`;
-            newLine.appendChild(newCell);
+            //
+            var newLine = document.createElement("tr");
+            var idLine = index;
+            newLine.classList.add("boardLine");
+            newLine.id = index;
+            pixelBoard.appendChild(newLine);
+
+            for (let index = 0; index < numberBoard; index++) {
+                var newCell = document.createElement('td');
+                newCell.classList.add("boardCell");
+                newCell.classList.add("black");
+                newCell.id = `line-${idLine}-cell-${index}`;
+                newLine.appendChild(newCell);
+            }
         }
-    }
-}
+    },
+    resetBoard() {
+        var board = document.getElementById('pixelBoard');
+        board.innerHTML = "";
+    },
 
-function resetBoard() {
-    var board = document.getElementById('pixelBoard');
-    board.innerHTML = "";
-}
-
-// on addEventListener pour détecter click
-// au clic on replace la class de la td si c'est blc > noir et inversement
-var boardCell = document.getElementsByClassName('boardCell');
-var invaderDiv = document.getElementById('pixelBoard');
-var arrayBoardCell = document.getElementsByClassName('boardCell');
+};
 
 pixelBoard.addEventListener('click', function (event) {
     var clickedCell = document.getElementById(event.target.id);
@@ -45,33 +67,11 @@ pixelBoard.addEventListener('click', function (event) {
     }
 })
 
-// recupérer le form et construire dedans les input et le bouton
-var headerForm = document.getElementById('header-form');
-console.log(headerForm);
-//construire les input
-var gridInput = document.createElement('input');
-gridInput.id = 'grid-input';
-gridInput.setAttribute('type', 'number');
-gridInput.setAttribute('placeholder', 'Taille de la grille');
-headerForm.appendChild(gridInput);
-
-var pixelInput = document.createElement('input');
-pixelInput.id = 'pixel-input';
-pixelInput.setAttribute('type', 'number');
-pixelInput.setAttribute('placeholder', 'Taille des pixels');
-console.log(pixelInput);
-headerForm.appendChild(pixelInput);
-
-//construire le bouton
-var formButton = document.createElement('button');
-formButton.id = "form-button";
-formButton.setAttribute('type', 'button');
-formButton.textContent = 'Valider';
-headerForm.appendChild(formButton);
-
 // event sur clic button qui récupère value des input
-formButton.addEventListener('click', function (event) {
-    resetBoard();
-    console.log(gridInput.value);
-    buildBoard(gridInput.value);
+app.formButton.addEventListener('click', function (event) {
+    app.resetBoard();
+    console.log(app.gridInput.value);
+    app.buildBoard(app.gridInput.value);
 })
+
+app.init();
